ae95fded4ca2057ed26c4373a7382da1e2df630e
Fesih Alpagu <alpagu.fesih@gmx.at>
2026-05-13 18:21:28 +0200

    feat:final version 01

--- script.js
+++ script.js
@@ -1,32 +1,273 @@
+let allPokemon = [];
 
-function init(){
+let currentIndex = 0;
 
-    loadPokemons();
+function init() {
+  loadPokemon();
 }
 
+let offset = 0;
+let limit = 24;
 
-const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
 
+async function loadPokemon() {
+  let BASE_URL = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+  document.getElementById("spinner").style.display = "flex";
+  let response = await fetch(BASE_URL);
+  let data = await response.json();
 
-async function loadPokemons() {
-    
-    let response = await fetch(BASE_URL);
-    let data = await response.json();
+  await renderPokemonDetails(data);
 
-    renderPokemons(data);
+  renderPokemon();
+  document.getElementById("spinner").style.display = "none";
+  offset += limit;
 }
 
+async function renderPokemonDetails(data) {
+  for (let i = 0; i < data.results.length; i++) {
+    let pokemon = data.results[i];
 
-function renderPokemons(data) {
-    let contentref = document.getElementById("content");
+    let pokemonDetails = await fetch(pokemon.url);
+    let pokemonDetailInfo = await pokemonDetails.json();
 
-    let html = "";
+    allPokemon.push(pokemonDetailInfo);
+  }
+}
 
-    for (let i = 0; i < data.results.length; i++) {
+function getAbilities(pokemon) {
+  let abilitiesText = "";
 
-        let pokemon = data.results[i];
-        html += getPokemonsTemplate(pokemon);  
+  for (let i = 0; i < pokemon.abilities.length; i++) {
+    abilitiesText += pokemon.abilities[i].ability.name;
+
+    if (i < pokemon.abilities.length - 1) {
+      abilitiesText += ", ";
+    }
+  }
+  return abilitiesText;
+}
+
+function getStats(pokemon) {
+  let statsHTML = "";
+
+  for (let i = 0; i < pokemon.stats.length; i++) {
+    let statInfo = pokemon.stats[i];
+
+    let label = firstLetterUpperCase(statInfo.stat.name).replace(":", "");
+    let value = statInfo.base_stat;
+
+    statsHTML += `
+    <div class="stat-row">
+      <span class="label">${label}</span>
+      <div class="bar">
+          <div class="fill" style="width: ${value}%"></div>
+      </div> 
+     
+    </div>
+    `;
+  }
+  return statsHTML;
+}
+
+function renderPokemon() {
+  let contentRef = document.getElementById("content");
+
+  let html = "";
+
+  for (let i = 0; i < allPokemon.length; i++) {
+    let pokemon = allPokemon[i];
+    html += getPokemonTemplate(pokemon);
+  }
+  contentRef.innerHTML = html;
+}
+
+function firstLetterUpperCase(name) {
+  if (name === "hp") return "HP";
+  return name[0].toUpperCase() + name.slice(1);
+}
+
+function getTypeColor(type) {
+  const colours = {
+    normal: "#A8A77A",
+    fire: "#EE8130",
+    water: "#6390F0",
+    electric: "#F7D02C",
+    grass: "#7AC74C",
+    ice: "#96D9D6",
+    fighting: "#C22E28",
+    poison: "#A33EA1",
+    ground: "#E2BF65",
+    flying: "#A98FF3",
+    psychic: "#F95587",
+    bug: "#A6B91A",
+    rock: "#B6A136",
+    ghost: "#735797",
+    dragon: "#6F35FC",
+    dark: "#705746",
+    steel: "#B7B7CE",
+    fairy: "#D685AD",
+  };
+
+  return colours[type] || "#ccc";
+}
+
+function getTypeIcon(type) {
+  const icons = {
+    normal: "./assets/icon_logos/normal.svg",
+    fire: "./assets/icon_logos/fire.svg",
+    water: "./assets/icon_logos/water.svg",
+    electric: "./assets/icon_logos/electric.svg",
+    grass: "./assets/icon_logos/grass.svg",
+    ice: "./assets/icon_logos/ice.svg",
+    fighting: "./assets/icon_logos/fighting.svg",
+    poison: "./assets/icon_logos/poison.svg",
+    ground: "./assets/icon_logos/ground.svg",
+    flying: "./assets/icon_logos/flying.svg",
+    psychic: "./assets/icon_logos/psychic.svg",
+    bug: "./assets/icon_logos/bug.svg",
+    rock: "./assets/icon_logos/rock.svg",
+    ghost: "./assets/icon_logos/ghost.svg",
+    dragon: "./assets/icon_logos/dragon.svg",
+    dark: "./assets/icon_logos/dark.svg",
+    steel: "./assets/icon_logos/steel.svg",
+    fairy: "./assets/icon_logos/fairy.svg",
+  };
+
+  return icons[type] || "icons/default.svg";
+}
+
+function renderTypes(pokemon) {
+  let html = "";
+
+  for (let i = 0; i < pokemon.types.length; i++) {
+    let type = pokemon.types[i].type.name;
+    let icon = getTypeIcon(type);
+    let color = getTypeColor(type);
+
+    html += `
+      <img class="icon" style="background-color: ${color}" src="${icon}" alt="${type}">
+    `;
+  }
+  return html;
+}
+
+document.getElementById("searchInput").addEventListener("input", searchPokemon);
+
+function searchPokemon() {
+  let input = document.getElementById("searchInput").value.toLowerCase();
+  let content = document.getElementById("content");
+
+  let html = "";
+
+  for (let i = 0; i < allPokemon.length; i++) {
+    let pokemon = allPokemon[i];
+
+    if (pokemon.name.toLowerCase().includes(input)) {
+      html += getPokemonTemplate(pokemon);
+    }
+  }
+  content.innerHTML = html;
+}
+
+async function openPopUp(id) {
+  let pokemon = allPokemon.find((p) => p.id === id);
+
+  currentIndex = allPokemon.findIndex((p) => p.id === id);
+
+  let popUp = document.getElementById("popUp");
+  popUp.innerHTML = getDialogTemplate(pokemon);
+
+  popUp.showModal();
+  popUp.focus();
+
+  await loadEvolutionChain(pokemon);
+}
+
+async function loadEvolutionChain(pokemon) {
+  let speciesResponse = await fetch(pokemon.species.url);
+  let speciesData = await speciesResponse.json();
+
+  let evolutionResponse = await fetch(speciesData.evolution_chain.url);
+  let evolutionData = await evolutionResponse.json();
+
+  let evolutions = [];
+
+  getEvolutionNames(evolutionData.chain, evolutions);
+  renderEvolutionChain(evolutions);
+}
+
+function getEvolutionNames(chain, evolutions) {
+  evolutions.push(chain.species.name);
+
+  if (chain.evolves_to.length > 0) {
+    getEvolutionNames(chain.evolves_to[0], evolutions);
+  }
+}
+
+function renderEvolutionChain(evolutions) {
   
-     } 
-       contentref.innerHTML = html;
+  let container = document.querySelector(".evolutionContainer");
+
+  let html = "";
+
+  for (let i = 0; i < evolutions.length; i++) {
+    let evoPokemon = allPokemon.find((p) => p.name === evolutions[i]);
+
+    if (evoPokemon) {
+      html += getEvosTemplate(evoPokemon, i, evolutions);
+    }
+  }
+  container.innerHTML = html;
 }
+
+let popUp = document.getElementById("popUp");
+
+popUp.addEventListener("click", (event) => {
+  if (event.target === popUp) {
+    popUp.close();
+  }
+});
+
+function selectTabs(index) {
+  let popUp = document.getElementById("popUp");
+
+  let tabs = popUp.querySelectorAll(".tab");
+  let infos = popUp.querySelectorAll(".info");
+
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].classList.remove("active");
+  }
+
+  for (let i = 0; i < infos.length; i++) {
+    infos[i].classList.remove("active");
+  }
+
+  tabs[index].classList.add("active");
+  infos[index].classList.add("active");
+}
+
+function arrowLeftBtn() {
+  if (currentIndex > 0) {
+    currentIndex--;
+  } else {
+    currentIndex = allPokemon.length - 1;
+  }
+  openPopUp(allPokemon[currentIndex].id);
+}
+
+function arrowRightBtn() {
+  if (currentIndex < allPokemon.length - 1) {
+    currentIndex++;
+  } else {
+    currentIndex = 0;
+  }
+  openPopUp(allPokemon[currentIndex].id);
+}
+
+popUp.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    arrowLeftBtn();
+  } else if (e.key === "ArrowRight") {
+    arrowRightBtn();
+  }
+});
--- scripts/template.js
+++ scripts/template.js
@@ -1,13 +1,86 @@
-function getPokemonsTemplate(pokemon){
-    return`
-            <div class="pokeCard">
+function getPokemonTemplate(pokemon) {
+  let mainType = pokemon.types[0].type.name;
+  let color = getTypeColor(mainType);
+
+  return `
+            <button class="pokeCard" onclick="openPopUp(${pokemon.id})">
                 <div class="nameContainer">
-                    <p>#${pokemon.id}</p>
-                    <h3>${pokemon.name}</h3>
+                    <p class="pokeTitle">#${pokemon.id}</p>
+                    <h3 class="pokeTitle">${firstLetterUpperCase(pokemon.name)}</h3>
                 </div>
-                <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemon.id}" alt="">
-                <button><img src="./assets/icon_logos/search_icon_white.svg"></button>
-          </div>
-       
+                <div class="imageBg" style="background-color: ${color}"> 
+                    <img class="pokeImg" src="${pokemon.sprites.front_default}" alt="${firstLetterUpperCase(pokemon.name)}">
+                </div>
+                <div>${renderTypes(pokemon)}</div>
+          </button> 
     `;
+}
+
+function getDialogTemplate(pokemon) {
+  let mainType = pokemon.types[0].type.name;
+  let color = getTypeColor(mainType);
+
+  return `
+        <div class="popUpCard">  
+
+            <div class="nameContainer">
+              <p class="popUpTitle">#${pokemon.id}</p>
+              <h3 class="popUpTitle">${firstLetterUpperCase(pokemon.name)}</h3>
+            </div>
+            <div class="popUpImageBg" style="background-color: ${color}"> 
+              <img class="pokePopUpImg" src="${pokemon.sprites.front_default}" alt="${firstLetterUpperCase(pokemon.name)}">
+            </div>
+            <div>${renderTypes(pokemon)}</div>
+    
+            <div class="pokeInfos">
+            
+                <div class="tabs">
+                    <button onclick="selectTabs(0)" class="tab active">main</button>
+                    <button onclick="selectTabs(1)" class="tab">stats</button>
+                    <button onclick="selectTabs(2)" class="tab">evo chain</button>
+                </div>
+            
+                <ul class="info active">
+                    <li><span class="label">Height</span><span class="value">: ${pokemon.height} m</span></li>
+                    <li><span class="label">Weight</span><span class="value">: ${pokemon.weight} kg</span></li>
+                    <li><span class="label">Abilities</span><span class="value">: ${getAbilities(pokemon)}</span></li>
+                    <li><span class="label">Base experience</span><span class="value">: ${pokemon.base_experience}</span></li>
+                </ul>
+
+                <ul class="info">
+                     ${getStats(pokemon)}
+                </ul>
+
+                <ul class="info evolutionContainer">
+                    
+                </ul>
+            </div>
+
+            <div class="arrows" id="arrows">
+              <button id="arrowLeft" onclick="arrowLeftBtn()">
+                <img src="./assets/icon_logos/pfeil-links.png" alt="arrow left" />
+              </button>
+              <button id="arrowRight" onclick="arrowRightBtn()">
+                <img src="./assets/icon_logos/pfeil-right.png" alt="arrow right" />
+              </button>
+            </div>
+
+        </div> 
+    `;
+}
+
+function getEvosTemplate(evoPokemon, i, evolutions) {
+
+  let html = `
+        <div class="evoCard">
+          <img class="evoImg"
+            src="${evoPokemon.sprites.front_default}"
+            alt="${evoPokemon.name}">
+          <p> ${firstLetterUpperCase(evoPokemon.name)}</p>
+        </div>
+      `;
+        if (i < evolutions.length - 1) {
+        html += `<img class="evoArrow" src="./assets/icon_logos/arrow_double.svg" alt="arrow left"/>`;
+    }
+    return html;
 }
